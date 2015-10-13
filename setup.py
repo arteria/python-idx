11b0a4bac2a27bc30a9ff2063156d9b034bb0508
Philippe O. Wagner <wagner@arteria.ch>
2015-10-13 13:34:51 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     packages=['idx', ],
     license='MIT',
     long_description=open('README.md').read(),
-    description='Python library used to interact with Homegate or Immoscout24 usind the IDX3.01 API.',
+    description='Python library used to interact with Homegate or Immoscout24 using the IDX3.01 API.',
     author='arteria GmbH',
     author_email='admin@arteria.ch',
 )
